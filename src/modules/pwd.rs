69a06f66b07671cd7c4d02f9ce7597ed2d19efdb
lqarnachi <4minedina@gmail.com>
2025-08-24 17:57:04 +0100

    pwd

---
+++ src/modules/pwd.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+pub fn pwd(args: &[String]) -> Result<String, String> {
+    if !args.is_empty() {
+        return Err("pwd: too many arguments".to_string());
+    }
+
+    match env::current_dir() {
+        Ok(path) => {
+            match path.to_str() {
+                Some(path_str) => Ok(path_str.to_string()),
+                None => Err("pwd: current directory path contains invalid UTF-8".to_string()),
+            }
+        }
+        Err(e) => Err(format!("pwd: error getting current directory: {}", e)),
+    }
+}
