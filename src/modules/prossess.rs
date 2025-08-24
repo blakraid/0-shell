5c223d144fe771f1cdc099ceb4bf9cc581cd617e
lqarnachi <4minedina@gmail.com>
2025-08-24 17:27:20 +0100

    mkdir

--- src/modules/prossess.rs
+++ src/modules/prossess.rs
@@ -1,10 +1,15 @@
 use crate::modules::ls::ls;
+use crate::modules::mkdir::mkdir;
+// use libc::mkdir;
 
-pub fn prossess(value : Vec<String>) -> Result<String,String>{
-
+pub fn prossess(value: Vec<String>) -> Result<String, String> {
     let cmd = value[0].clone();
     match cmd.as_str() {
         "ls" => match ls(&value[1..]) {
+            Ok(s) => Ok(s),
+            Err(e) => Err(e),
+        },
+        "mkdir" => match mkdir(&value[1..]) {
             Ok(s) => Ok(s),
             Err(e) => Err(e),
         },
@@ -17,7 +22,6 @@ pub fn prossess(value : Vec<String>) -> Result<String,String>{
         "mv" => Ok("exit".to_string()),
         "mkdir" => Ok("exit".to_string()),*/
         "exit" => Ok("exit".to_string()),
-        _ => Err("Write a valid Command".to_string())
+        _ => Err("Write a valid Command".to_string()),
     }
 }
-
